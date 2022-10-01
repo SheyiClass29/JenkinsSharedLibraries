413f213d93572b5b8bcd733a4f62178de5a29a76
Pio7 <111322329+kelafat007@users.noreply.github.com>
2022-10-01 11:04:02 -0400

    Update common.groovy

--- vars/common.groovy
+++ vars/common.groovy
@@ -12,4 +12,10 @@ def call(String stageName){
      {
        sh "mvn deploy"
      }
+  else if ("${stageName}" == "Deploy To UAT")
+  {
+    sh "echo 'deploy to UAT'  "
+    deploy adapters: [tomcat8(credentialsId: 'Tomcat-credentials', path: '', url: 'http://172.31.10.131:8080')], contextPath: null, onFailure: false, war: 'target/*war'
+  }
 }
+
